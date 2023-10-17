670c9267488bbe2f60af78eb8df1147b3b2f9b4c
MahiHailu <hail.mulat@gmail.com>
2023-10-17 03:38:18 -0700

    databse commit

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Create database hbtn_0c_0 on the MySQL server.
+CREATE DATABASE for `hbtn_0c_0`; if it does not exist.
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+– Delete database hbtn_0c_0 on the MySQL server.
+DROP DATABASE if 'hbtn_0c_0' exists.
