787b20b423f3934d7fb351d7b94abd613db9a97b
MahiHailu <hail.mulat@gmail.com>
2023-11-14 07:06:46 -0800

    javascript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const { argv } = require('process');
-const conversion = Number(argv[2]);
+const converted = Number(argv[2]);
 if (isNaN(converted)) { console.log('Not a number'); } else { console.log(`My number: ${converted}`); }
