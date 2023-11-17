623b70a15c6c35bfe452038adc5fc283c7b7c04c
MahiHailu <hail.mulat@gmail.com>
2023-11-17 05:06:45 -0800

    new commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Listing states in database"""
+"""lists all states from the database hbtn_0e_0_usa
  Usage: ./0-select_states.py <mysql username>
                             <mysql password>
                             <database name >
