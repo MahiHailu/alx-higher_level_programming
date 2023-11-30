25dbcc6041a3d204f61e7a4c25dba9edf7f89f09
MahiHailu <hail.mualt@gmail.com>
2023-11-30 07:31:49 -0800

    new commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Takes in a URL, sends a request that to URL,displays the size of the body of the response
-curl -s "$1" | wc -ci
+# Bash script that takes in a URL, sends a request to that URL and displays the size
+curl -s "$1" | wc -c
